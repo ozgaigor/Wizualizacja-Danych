9df6c9a2c6221d9c73a9a6a9dab6ce7e50def7b8
ozgaigor <73592635+ozgaigor@users.noreply.github.com>
2021-03-10 16:58:23 +0100

    Delete Zadania 1 directory

--- Zadania 1/2.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Podaj liczbe a: "))
-print("{a}".format(a=a))
-
-b = int(input("Podaj liczbe b: "))
-print("{b}".format(b=b))
-
-print(a*b)
--- Zadania 1/4.py
+++
@@ -1,4 +0,0 @@
-a = int(input("Podaj liczbe a: "))
-print("{a}".format(a=a))
-
-print('Wartoscia absolutna liczby a jest:', abs(a))
