1aca35f3a7b09f9cd3f6d9e9c679ddb10de0f2b6
ozgaigor <73592635+ozgaigor@users.noreply.github.com>
2021-05-05 11:55:02 +0200

    Update 8_1.py

--- lab8/8_1.py
+++ lab8/8_1.py
@@ -2,6 +2,6 @@ import pandas as pd
 import xlrd
 import openpyxl
 
-df = pd.read_excel(pd.ExcelFile("Datasets/imiona.xlsx"), "Arkusz1")
+df = pd.read_excel(pd.ExcelFile("lab8\imiona.xlsx"), "Arkusz1")
 
 print(df)
